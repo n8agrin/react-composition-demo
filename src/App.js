1caa7d315fcf71eefcd39b55982c6be72c23cd59
Nathan Agrin <n8agrin@gmail.com>
2016-10-13 10:23:42 -0700

    Show 'watching' of prop updates in react

--- src/App.js
+++ src/App.js
@@ -23,17 +23,20 @@ class App extends Component {
         <p className="App-intro">
           Click the Button Below!
         </p>
-        <ClickMeButton action={this.incrementCounter.bind(this)} doubleValue={this.state.counter * 2}></ClickMeButton>
+        <ClickMeButton action={this.incrementCounter.bind(this)} doubleValue={this.state.counter}></ClickMeButton>
       </div>
     );
   }
 }
 
 class ClickMeButton extends Component {
+  componentWillUpdate(nextProps, nextState) {
+    console.log('updating double to', nextProps.doubleValue);
+  }
   render () {
     return (
       <div>
-      <h2>Double! {this.props.doubleValue}</h2>
+      <h2>Double! {this.props.doubleValue * 2}</h2>
       <button onClick={this.props.action}>Click Me!</button>
       </div>
     )
